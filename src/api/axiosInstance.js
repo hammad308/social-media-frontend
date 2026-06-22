95a8b0747e6a8adcd2da60c99a076b692d75216b
Muhammad Hammad Ali <mh6139251@gmail.com>
2026-06-22 17:23:55 +0500

    chore: Update axios instance to use environment variable for base URL

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const axiosInstance=axios.create({
-    baseURL:"http://localhost:3000/api"
+    baseURL:import.meta.env.VITE_API_URL
 });
 
 axiosInstance.interceptors.request.use(
