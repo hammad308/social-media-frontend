5348754e47b151abbef9001c1ae7715b6fa05205
Muhammad Hammad Ali <mh6139251@gmail.com>
2026-06-22 23:12:51 +0500

    fix: Update socket connection URL to use the correct backend endpoint

--- src/socket.js
+++ src/socket.js
@@ -1,7 +1,7 @@
 import {io} from "socket.io-client";
 
 const socket=io(
-    "https://social-media-frontend-ochre-five.vercel.app"
+    "https://social-media-backend-m65i.onrender.com"
 );
 
 export default socket;
